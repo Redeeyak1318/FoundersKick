19f53a55d689e5ec9c9a6494454b6aa02abedb3b
Raktim Sonowal <raktimsonowal28197@gmail.com>
2026-02-06 00:14:15 +0530

    fix google oauth callback url

--- server.js
+++ server.js
@@ -173,7 +173,7 @@ const authenticateToken = (req, res, next) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback",
+    callbackURL: "https://founderskick.onrender.com/auth/google/callback",
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
